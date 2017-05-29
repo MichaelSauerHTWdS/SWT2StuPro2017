54b5fc8e7c3f67820707f29c2458bfd623adf49e
Marc Willms <mwillms@htwsaar.de>
2017-05-29 13:25:21 +0200

    Add files via upload
    nicht das richtige Verzeichnis aber besser.

---
+++ Waren.java
@@ -0,0 +1,25 @@
+public enum Waren{
+    0("Tuche"), 
+    1("Stockfisch"), 
+    2("Bier"),
+    3("Wein"),
+    4"Zinn"),
+    5("Salz"),
+    6("Pelze"),
+    7("Kupfer");
+  
+    private String warenName;
+    public static int warenAnzahl = Waren.values().length;
+  
+    Waren(int warenIndex){
+        this.warenName=warenIndex;
+     }
+  
+    public String getwarenName(){
+      return warenName;
+    }
+  
+      public int getwarenAnzahl(){
+      return warenAnzahl;
+    }
+};
