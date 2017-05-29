506c04d28e78711316335a5840a2f8befdcb47b6
Marc Willms <mwillms@htwsaar.de>
2017-05-29 11:33:04 +0200

    Create lager.java
    erstellen Lagerklasse:
    ToDo: Enums Waren, Handelsorte ; Idee: Enumklasse. (Tage, Orte, Waren, Schiffstypen) etc. das macht alles modularer
    weiter: Klasse Kontor.

---
+++ lager.java
@@ -0,0 +1,12 @@
+public class Lager{
+  private int lagerMengeMin;
+
+  //Min= Sperrbestand bei dem z.B. beim Kontor wieder waren dazugebucht werden muessen 
+  //(Vorgabe Kontor unendlich Waren eines Typs)
+  
+  private int lagerMengeMax;
+  //Max= maximale kapazitaet eines Lagers, nach deren ueberschreiten werden entweder 
+  //keine Waren mehr akzeptiert oder nicht mehr verbucht
+
+
+}
