32acc149eaafe5f07e935906dcf83ff510d0492a
Marc Willms <mwillms@htwsaar.de>
2017-05-29 14:31:57 +0200

    Create Spieler.java

---
+++ Spieler.java
@@ -0,0 +1,12 @@
+public class Spieler{
+- String Spieler[fortlaufendeNummer, gleichzeitig Reihenfolge]
+- String Spielerhafen [Starthafen, ggf. Hauptkontor]
+- boolean Begrüsst (Indikator Spielbeginn Spieler)
+- boolean bankrott
+- Schiffliste
+- Kontore mit individuellen Füllständen
+- Kontostand
+
+
+
+}
